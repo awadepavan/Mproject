c1d4e12887d8a3a0dc7b5f019bfa9bcba2bc776f
awadepavan <132901256+awadepavan@users.noreply.github.com>
2023-08-21 21:30:29 +0530

    mp

---
+++ EmployeesController/Controllers/EmployeesController.cs
@@ -0,0 +1,92 @@
+﻿using EmployeesController.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesController.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            List<Employee> lstEmp = Employee.GetAllEmployee();
+            return View(lstEmp);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee obj,IFormCollection collection)
+        {
+            try
+            {
+                Employee.InsertEmployee(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee obj,int id, IFormCollection collection)
+        {
+            try
+            {
+                Employee.UpdateEmployee(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee obj = Employee.GetSingleEmployee(id);
+            return View(obj);
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Employee.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
